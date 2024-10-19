87b48c796e83f13cdad00e25b89575ad8bf00a77
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-19 23:37:41 +0300

    Sgin up down section components

---
+++ resources/views/components/sign-up.blade.php
@@ -0,0 +1,24 @@
+<div class="bg-white p-8 pt-3">
+    <div class="max-w-xl mx-auto text-center">
+        <h2
+            class="text-gray-800 text-5xl font-extrabold relative after:absolute after:-bottom-5 after:h-1 after:w-1/2 after:bg-blue-600 after:left-0 after:right-0 after:mx-auto after:rounded-full">
+            Find new podcasts,
+            <span class="block text-2xl">
+                Grow your audience
+            </span>
+        </h2>
+        <div class="mt-12">
+            <p class="text-gray-800 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan,
+                nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.
+            </p>
+        </div>
+
+        <div class="flex max-sm:flex-col justify-center gap-6 mt-12">
+            <button type="button"
+                class="min-w-[140px] rounded px-4 py-2.5 text-sm tracking-wider font-semibold outline-none border text-white border-blue-600 bg-blue-600 hover:bg-transparent hover:text-blue-600 transition-all duration-300">
+                Sign up and join our beta access
+            </button>
+        </div>
+    </div>
+</div>
