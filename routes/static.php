4c27deaeefd26bc3d90e2266a312fb894b77a316
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-20 19:44:28 +0300

    Pricing Route

--- routes/static.php
+++ routes/static.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view("/", "static.pages.home")->name("home");
 Route::view("about", "static.pages.about")->name("about");
+Route::view('pricing', 'static.pages.pricing')->name('pricing');
